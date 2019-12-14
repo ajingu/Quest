3c5695621a9a9f8f039306fd06f154203dc9fc4c
Arata Jingu <guujin0120@gmail.com>
2019-12-14 14:02:34 +0900

    自動ビルド用のスクリプトを作った

---
+++ Assets/Editor/Build/Builder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Builder
+{
+    private static readonly string[] Scenes = FindEnabledEditorScenes();
+
+    private const string AppName = "Quest";
+    private static readonly string BuildDir = System.IO.Directory.GetCurrentDirectory() + "/Build";
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        var editorScenes = new List<string>();
+        
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            editorScenes.Add(scene.path);
+        }
+
+        return editorScenes.ToArray();
+    }
+
+    private static void GenericBuild(string[] scenes, string targetPath, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        BuildPipeline.BuildPlayer(scenes, targetPath, buildTarget, buildOptions);
+    }
+    
+    [MenuItem("Custom/CI/Build Windows")]
+    public static void BuildWindows()
+    {
+        var targetPath = BuildDir + "/" + AppName + ".exe";
+        GenericBuild(Scenes, targetPath, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+}
