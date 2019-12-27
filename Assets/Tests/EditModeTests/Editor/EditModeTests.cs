6597bda619ad23627e0c72c7d92c7dfb057f861c
Arata Jingu <guujin0120@gmail.com>
2019-12-27 23:35:24 +0900

    Add EditMode Test Code

---
+++ Assets/Tests/EditModeTests/Editor/EditModeTests.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Model;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class EditModeTests
+    {
+        [Test]
+        public void UserTest()
+        {
+            var user = new User(0, "Alice", false);
+            
+            Assert.AreEqual(0, user.Id);
+            Assert.AreEqual("Alice", user.Name.Value);
+            Assert.AreEqual(false, user.IsPaid.Value);
+        }
+    }
+}
